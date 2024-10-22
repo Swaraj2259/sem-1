b747104a3ee1d163fc919ca6a1ecde63a40b8c4a
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-10-23 00:34:02 +0530

    added oct 22

---
+++ octuber_22.py
@@ -0,0 +1,22 @@
+# A function to find factorial of numbers from 1 to 4
+'''
+def fact(num):
+    result=1
+    while num>=1:
+        result=result*num
+        num=num-1
+    return result
+for i in range (1,5):
+    print("factorial of number:",i,"is",fact(i))
+'''
+#A function to find factorial of numbers from 1 to 4 here is example
+'''
+def sum_sub(a,b):
+    sum=a+b
+    sub=a-b
+    return sum,sub
+x,y=sum_sub(345,658)
+print("sum of a and b is:",x) 
+print("sub of a and b is :",y)
+'''
+
