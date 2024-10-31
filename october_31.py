374ed7bba7f67ab800ca6343c26e70d83ff7f226
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-10-31 22:55:46 +0530

    added

---
+++ october_31.py
@@ -0,0 +1,23 @@
+import math
+print(math.ceil(1.4))#o/p:2
+print(math.ceil(2.3))
+
+print(math.fabs(-66.3)) #66.3
+print(math.fabs(-7)) #7
+
+print(math.factorial(26))
+print(math.factorial(4))
+
+print(math.floor(0.6))#0
+print(math.floor(-5.3))#-6
+
+#returns the remendier of x divided y
+print(math.fmod(12,2))#0.0
+print(math.fmod(12,5))#2.0
+
+print(math.prod((12,2)))
+
+print(math.pow(12,4))
+
+print(math.sqrt(12))
+
