594533a3e27d8bad572d7d032dc4ee608bce4c26
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-04 10:51:52 +0530

    added

---
+++ Nov_3.py
@@ -0,0 +1,16 @@
+#OPPS
+# class car:
+#     colour="pink"
+#     brand="range rover"
+# Car1=car()
+# print(" colour of car is ",Car1.colour)
+# print(" brand of car is ",Car1.brand)
+
+
+
+
+
+
+
+
+
