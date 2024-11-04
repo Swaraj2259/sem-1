793418d1901de874786f73cb039d0d14ae730f7b
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-04 11:24:06 +0530

    modifed

--- Nov_3.py
+++ Nov_3.py
@@ -6,7 +6,23 @@
 # print(" colour of car is ",Car1.colour)
 # print(" brand of car is ",Car1.brand)
 
+class student:
+    
+    #default constructors
+    def __init__(self) : 
+        print("adding new student to the new database")
 
+    def __init__(self,name, marks) : #__init__ is the constructors
+        self.name=name
+        self.marks=marks
+        print("adding new student to the new database")
+
+s1=student("shravani",23)
+print(s1.name,s1.marks)
+
+
+s2=student("mahesh",41)
+print(s2.name,"is the of the student and he/her marks =",s2.marks)
 
 
 
