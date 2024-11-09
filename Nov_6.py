ce8c6405c9bbf6c684544221d2df5852c2f77487
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-09 11:43:19 +0530

    added

--- Nov_6.py
+++ Nov_6.py
@@ -19,20 +19,20 @@
 # s2.welcome()
 
 
-# class student:
+class student:
 
-#     def __init__(self,name,marks):
-#         self.name=name
-#         self.marks=marks
-#     def get_avg(self):
-#         sum=0
-#         for val in self.marks:
-#             sum+= val
-#         print(self.name,"your avg score is ",sum/3)
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def get_avg(self):
+        sum=0
+        for val in self.marks:
+            sum+= val
+        print(self.name,"your avg score is ",sum/3)
 
 
-# s1=student("swaraj",[79,89,99])
-# s1.get_avg()
+s1=student("swaraj",[79,89,99])
+s1.get_avg()
 
         
 
