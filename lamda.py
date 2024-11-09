db50ed65762336050665f5e88c0d0c4b9e74b384
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-09 11:42:35 +0530

    added

---
+++ lamda.py
@@ -0,0 +1,15 @@
+# # lambda is keyword
+# def square(a):
+#     return a*a 
+# print(square(5))
+# #example od lamda fun
+# s=lambda n: n*n
+# print(s(2))
+import tkinter as mp
+root = mp.Tk()
+root.geometry('500x500')
+
+root.title("my first tkiner app")
+s=mp.Entry(root)
+s.pack()
+root.mainloop() 
