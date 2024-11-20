eec1208ce4fa2d77280d6bd99ad8569120fbd38b
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-20 19:42:23 +0530

    added

---
+++ resume.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main () {
+// resume code
+cout<<"Name:\t Swaraj\n";
+cout<<"Age:\t 19\n";
+cout<<"adress:\t itm,skill university\n";
+cout<<"no.:\t 7820858979\n";
+cout<<"email:\t swarajmahesh83@gmail.com\n";
+}
+
+
+
+
+
+
+
+
+
+
+
