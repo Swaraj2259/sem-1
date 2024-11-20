e678e0de944117e5797956428c7c95340722fb09
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-20 19:59:22 +0530

    added

---
+++ c++/code.cpp
@@ -0,0 +1,68 @@
+// #include <iostream> // Required for input/output
+// using namespace std;
+
+// int main() {
+//     cout << "Hello, World!" << endl; // Output to the console here endl is for reomving the cursor to the next line
+//     return 0; // Indicates successful execution
+// }
+// #include <string> // Required for input/output
+// using namespace std;
+// int main() {
+// int x = 5;          // Integer
+// float y = 3.14;     // Float
+// string name = "Swaraj"; // String (requires #include <string>)
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     string name;
+//     cout << "Enter your name: ";
+//     cin >> name;
+//     cout << "Hello " << name << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main () {  // main function is the entry point of the program
+//     string name;
+//     cout <<"entre u are name :" ;
+//     cin >> name;
+//     cout << "hello "<< name <<"\n";
+//     int age=19;
+//     cout << "u are age is "<< age <<"\n";
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main () {
+    int a,b;
+    cout<<"entre the value of a"<<"\n"<<"and b: ";
+    cin>> a >> b ;
+
+    int sum = a+b;
+    cout<< sum<<" - is sum"<< endl;
+
+    int avg = sum/2;
+    cout<< avg<<" - is the avg"<< endl;
+    return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     int b, a, c;
+//     cout<< b<< endl<< a << endl<< c << endl;
+//     cin>> b >> a >> c;
+//     return 0;
+// }
+ 
+
+
+
+
+
+
