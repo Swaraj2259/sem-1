f4ff21a0d48792acd21ae15d89b2d3343d8bfef0
swaraj wattamwar <swarajwattamwar@swarajs-MacBook-Air.local>
2024-11-20 19:41:53 +0530

    added

---
+++ hello_world.cpp
@@ -0,0 +1,64 @@
+// #include <iostream>
+// using namespace std;
+
+// int main () {  // main function is the entry point of the program
+// string name;
+// cout <<"entre u are name :" ;
+// cin >> name;
+// cout << "hello "<< name <<"\n";
+// int age=12;
+// cout << "u are age is "<< age <<"\n";
+// return 0;
+// }
+#include<iostream>
+using namespace std;
+// int main (){
+//     int roll_no;
+//     string name;
+//     float fees;
+//     long contact;
+//     cout<<"entre the roll no., name, fees, contact no. : ";
+//     cin>>roll_no>>name>>fees>>contact;
+
+//     cout<<"======================================\n";
+//     cout<< "\t student details"<<endl;
+//     cout<<"======================================\n";   
+//     cout<<"\nroll_no: "<<roll_no;
+//     cout<<"\nname: "<<name;
+//     cout<<"\nfees: "<<fees;
+//     cout<<"\ncontact: "<<contact<<endl;
+//     return 0;
+// }
+int main (){
+    // //arithmatic operators
+    // cout<<5+5<<endl;
+    // cout<<5-5<<endl;
+    // cout<<5*5<<endl;
+    // cout<<5/3<<endl;
+    // cout<<5%5<<endl;
+
+
+
+    // //relational operators
+    // cout<<(4==5)<<endl;
+    // cout<<(5!=5)<<endl;
+    // cout<<(4>5)<<endl;
+    // cout<<(4<5)<<endl;
+    // cout<<(4>=5)<<endl;
+    // cout<<(5<=4)<<endl;
+    // logical operators
+    cout<<(4>5 && 5<=9)<<endl;
+    cout<<(4<=5 || 5<=9)<<endl;
+    cout<<!(4>5)<<endl;
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
