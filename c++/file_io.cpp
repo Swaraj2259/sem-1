f53bdbf0973155a9851760be4c98b4e2551b6ee0
swaraj wattamwar <swarajmahesh83@gmail.com>
2024-12-13 23:51:44 +0530

    added

---
+++ c++/file_io.cpp
@@ -0,0 +1,85 @@
+// #include<iostream>
+// #include<fstream>
+// #include<string>
+// using namespace std;
+// int main(){
+    // string line;
+    // ifstream myfile("pra.txt");
+    // if(!myfile){
+    //     cout << "error in opeaning";
+    //     return 1;
+    //     }
+    //     while (getline(myfile,line))
+    //     {
+    //         cout<<line<<'\n';
+    //     }
+        
+    //     // myfile <<"this is a line\n";
+    //     // myfile <<"this is another line\n";
+    //     myfile.close();
+    // }
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// int main()
+// {
+//     ifstream file("para.txt");
+//     ofstream copy("pra.txt");
+
+//     if (!file)
+//     {
+//         cout << "Error opening file" << endl;
+//     }
+
+//     string line;
+//     while (getline(file, line))
+//     {
+//         cout << line << endl;
+//         copy << line << endl;
+
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     string line;
+//     fstream file;
+
+//     file.open("para.txt", ios::app);
+//     file<<"this is a line\n";
+//     file<<"tuza nana che\n"; 
+//     file.close();
+//     file.open("para.txt", ios::in);
+//     while(getline(file, line)){
+//         cout<<line<<endl;
+//     }
+//     file.close();
+// }
+int main (){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
